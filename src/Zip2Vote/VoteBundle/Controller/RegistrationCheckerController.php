e7da73dca53c3f0561c8a4d4b8aff3f91416f5b8
Cameron <cameronmanley@sbcglobal.net>
2014-09-27 16:11:26 -0500

    Changed some doc blocks on the RegistrationCheckerController.php

--- src/Zip2Vote/VoteBundle/Controller/RegistrationCheckerController.php
+++ src/Zip2Vote/VoteBundle/Controller/RegistrationCheckerController.php
@@ -2,8 +2,20 @@
 
 use Goutte\Client;
 
+/**
+ * RegistrationChecker controller.
+ *
+ * @Route("/Register")
+ */
 class RegistrationCheckerController extends Controller {
 
+    /**
+     * Check if user is registered
+     *
+     * @Route("/", name="register")
+     * @Method("GET")
+     * @Template()
+     */
     public function indexAction() {
 
         $client = new Client();
