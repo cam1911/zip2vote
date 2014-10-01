36dcaa6ab0bb464ab5c93f6c0a2db1ab4595fc18
Jonathan <jonathan@thephpsolution.com>
2014-10-01 13:39:22 -0500

    Fixed the registration checker to load the form page.

--- src/Zip2Vote/VoteBundle/Controller/RegistrationCheckerController.php
+++ src/Zip2Vote/VoteBundle/Controller/RegistrationCheckerController.php
@@ -24,20 +24,39 @@ class RegistrationCheckerController extends Controller {
     public function indexAction() {
 
         $client = new Client();
+        $client->getClient()->setDefaultOption('config/curl/' . \CURLOPT_SSL_VERIFYPEER, false);
+        $client->getClient()->setDefaultOption('config/curl/' . \CURLOPT_SSL_VERIFYHOST, false);
         $crawler = $client->request('GET', 'https://team1.sos.state.tx.us/voterws/viw/faces/SearchSelectionVoter.jsp');
-        $client->getClient()->setDefaultOption('config/curl/' . \CURLOPT_TIMEOUT, 60);
-        $form = $crawler->selectButton('Name')->form();
+        $form = $crawler->selectButton('Next (Siga) >')->form();
+        
+        /*
+         * To determine what fields I needed to supply, I put this here and
+         *  looked at the output. Alternatively, I went to the url in Chrome and
+         *  hit CTRL+SHIFT+j. I clicked the Network tab and clicked the block
+         *  sign to clear the contents. I checked the radio to register and 
+         *  clicked the button. I inspected the headers of the request:
+         * 
+         * form1:radio1:N
+            form1:button1:Next (Siga) >
+            com.sun.faces.VIEW:_id58050:_id58051
+            form1:form1
+         */
+//        var_dump($form->all());
         $crawler = $client->submit($form, array(
-            'County' => 'Dallas',
-            'Last Name' => 'Manley',
-            'First Name' => 'Cameron',
-            'Date of Birth' => '06241994',
-            'ZIP Code' => '75080'
+            'form1:radio1' => 'N',
+            'form1:button1' => 'Next (Siga) >',
+            'form1' => 'form1',
                 )
         );
+        
+        /**
+         * I did this to verify the page I was receiving was the right one..
+         */
+//        var_dump($crawler->html());
         $crawler->filter('.flash-error')->each(function ($node) {
                     print $node->text() . "\n";
                 });
+                die('done');
     }
 
 }
